8d594ed35b6b687b97581d792f2a5fb789d6a910
mflanders06 <mflanders06@gmail.com>
2021-10-22 21:07:16 -0600

    image delete is setup but axios call is broken

---
+++ src/components/imageList.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function ImageList(){
+
+    const [images, setImages] = useState();
+
+    let getImages = async() => {
+        await axios
+            .get('/api/image_list')
+            .then(images => {
+                setImages(images.data);
+                //console.log('Another console', images);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    useEffect(() => {
+        getImages();
+
+    },[]);
+
+    let delImage = async(key) => {
+        await axios
+            .delete('/api/image_del', { key })
+            .then(console.log('Deleted: ', { key }))
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    function displayImages (ImageList){
+        console.log(ImageList);
+        return(
+            <div className="imageList">
+                {ImageList.map((value, index) => (
+                    <div className="adminImages" key={value.photo}>
+                        <img className="adminImage" src={"/images/" + value.photo} alt={value.imgdesc}></img>
+                        <div className="photoDesc">{value.imgdesc}</div>
+                        <button onClick={() => delImage(value.photo)}>Delete</button>
+                    </div>
+                ))}
+            </div>
+        )
+
+    }
+
+    
+    if(images && !(ImageList === undefined)){
+        return(
+            <div>
+                    {displayImages(images)}
+            </div>
+        );
+    } else { 
+        return(<></>);
+    }
+}
+
+export default (ImageList);
